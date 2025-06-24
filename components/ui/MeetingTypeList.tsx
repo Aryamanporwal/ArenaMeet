675236c5b00ebae861ff3d0c523ba938da71e495
Aryamanporwal <aryaman.3307@gmail.com>
2025-06-24 13:06:57 +0530

    Schedule Meeting Begin

--- components/ui/MeetingTypeList.tsx
+++ components/ui/MeetingTypeList.tsx
@@ -7,6 +7,9 @@ import { useStreamVideoClient } from '@stream-io/video-react-sdk';
 import { useUser } from '@clerk/nextjs';
 import { Call } from '@stream-io/video-react-sdk';
 import { toast } from "sonner";
+import { Textarea } from './textarea';
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 const MeetingTypeList = () => {
   const router = useRouter();
   const [meetingState , setMeetingState] = useState<'isScheduleMeeting'|'isJoiningMeeting'|'isInstantMeeting'|undefined>()
@@ -72,19 +75,85 @@ const MeetingTypeList = () => {
         description="Plan a meeting"
         handleClick={() => setMeetingState('isScheduleMeeting')}
         style={{backgroundColor: '	#2563EB'}}
-        />
+      />
       <HomeCard
         img="/icons/recordings.svg"
         title="View Recordings"
         description="Check out your recordings"
         handleClick={() => router.push('/recordings')}
-        style={{ backgroundColor: '	#1D4ED8' }} />
+        style={{ backgroundColor: '	#1D4ED8' }} 
+      />
       <HomeCard
         img="/icons/join-meeting.svg"
         title="Join Meeting"
         description="via invitation link"
         handleClick={() => setMeetingState('isJoiningMeeting')}
-        style={{backgroundColor: '#1E40AF'}}/>
+        style={{backgroundColor: '#1E40AF'}}
+      />
+
+
+      {!callDetails ?(
+          <MeetingModal
+          isOpen = {meetingState ==='isScheduleMeeting'}
+          onClose = {() => setMeetingState(undefined)}
+          title = "Create Meeting"
+          handleClick = {createMeeting}
+        > 
+          <div className = "flex flex-col gap-2.5">
+            <label className ="text-base text-normal leading-[22.4px] text-cyan-200">
+              Add a Description
+            </label>
+            <Textarea className = "border-none bg-gray-700 focus-visible:ring-0 focus-visible:ring-offset-0"
+            onChange = {(e)=>
+              setValues({...values, description:e.target.value})
+            }
+            />
+          </div>
+          <div className = "flex w-full flex-col gap2.5">
+            <label className ="text-base text-normal leading-[22.4px] text-cyan-200">
+              Select Date and Time
+            </label>
+            <ReactDatePicker
+              selected = {values.dateTime}
+              onChange = {(date)=> setValues({...values, dateTime: date!})}
+              showTimeSelect
+              timeFormat = "HH:mm"
+              timeIntervals = {15}
+              timeCaption = "time"
+              timeCaption="time"
+              dateFormat= "MMMM d, yyyy h:mm aa"
+              className = "w-full rounded bg-gray-950 p-2 focus:outline-none"
+            />
+          </div>
+        ):(
+          <MeetingModal
+          isOpen = {meetingState ==='isScheduleMeeting'}
+          onClose = {() => setMeetingState(undefined)}
+          title = "Meeting Created"
+          className = "text-center"
+          handleClick = {()=>{
+            // navigator.clipboard.writeText(meetinglink);
+            toast("Title of Link Copied")
+          }}
+          image = "/icons/checked.svg"
+          buttonIcon = "/icons/copy.svg"
+          buttonText= "Copy Meeting Link"
+        />
+      )}
+      <MeetingModal
+        isOpen={meetingState === 'isJoiningMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Type the link here"
+        className="text-center"
+        buttonText="Join Meeting"
+        handleClick={() => router.push(values.link)}
+      >
+        <Input
+          placeholder="Meeting link"
+          onChange={(e) => setValues({ ...values, link: e.target.value })}
+          className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+        />
+      </MeetingModal>
 
         <MeetingModal
           isOpen = {meetingState ==='isInstantMeeting'}
@@ -93,9 +162,8 @@ const MeetingTypeList = () => {
           className = "text-center"
           buttonText = "Start Meeting"
           handleClick = {createMeeting}
-          
-        />
-    </section>
+          />
+      </section>
   );
 };
 
